f414958438db4626f83868c84b2a1d6a8e9b9753
Maclean Farricy <mfarricy19@hhs-106-003.hanoverstudents.org>
2018-12-13 13:30:05 -0500

    project 3

---
+++ project3/scripts.js
@@ -0,0 +1,33 @@
+        function add() {
+            var value1 = parseInt(document.getElementById("value1").value);
+            var value2 = parseInt(document.getElementById("value2").value);
+
+            var result = value1 + value2;
+
+            document.getElementById("result").innerHTML = result;
+        }
+
+        function subtract() {
+            var value1 = parseInt(document.getElementById("value1").value);
+            var value2 = parseInt(document.getElementById("value2").value);
+
+            var result = value1 - value2;
+
+            document.getElementById("result").innerHTML = result;
+        }
+        function multiply() {
+            var value1 = parseInt(document.getElementById("value1").value);
+            var value2 = parseInt(document.getElementById("value2").value);
+
+            var result = value1 * value2;
+
+            document.getElementById("result").innerHTML = result;
+        }
+        function divide() {
+            var value1 = parseInt(document.getElementById("value1").value);
+            var value2 = parseInt(document.getElementById("value2").value);
+
+            var result = value1 / value2;
+
+            document.getElementById("result").innerHTML = result;
+        }
